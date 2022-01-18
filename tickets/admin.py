1d1238316f83591a8eb72ed403d1ce0bea1baa10
sbtah <shisastore@protonmail.com>
2022-01-18 20:38:24 +0100

    Initialized project and ticket model

---
+++ tickets/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from tickets.models import Ticket
+
+admin.site.register(Ticket)
---
+++ tickets/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
+
+
+class Ticket(models.Model):
+    """Model for user's tickets."""
+
+    
+    class Importance(models.TextChoices):
+        low = "It's anoying."
+        mid = "It slows me down."
+        high = "I can't work."
+        very_high = "Critial."
+    
+    class Status(models.TextChoices):
+        created = 'CREATED'
+        picked_up = 'PICKED UP'
+        extended = 'EXTENDED'
+        finished = 'FINISHED'
+
+
+
+    title = models.CharField(help_text=_('Title of problem'), max_length=100)
+    description = models.TextField(help_text=_('Description of problem.'))
+    screenshot = models.FileField(upload_to='worksheets')
+    importance = models.CharField(max_length=20, choices=Importance.choices)
+    created_by = models.CharField(max_length=20)
+    status = models.CharField(
+        max_length=10,
+        choices=Status.choices,
+        default=Status.created
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('tickets:ticket-details', args=[self.id])
+    
+    def __str__(self):
+        return f"ID:{self.id} Title:{self.title} Created by:{self.created_by} Status:{self.status}"
