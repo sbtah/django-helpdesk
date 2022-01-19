0fa67e06959cf62f674ac6392771fbe3b691c50b
sbtah <shisastore@protonmail.com>
2022-01-19 20:48:55 +0100

    Added custom UserAdmin

--- core/settings.py
+++ core/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tickets',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
+
