484e658d018c7cc2af2f7c949c0da41ebd2141ec
sbtah <shisastore@protonmail.com>
2022-01-28 21:14:54 +0100

    Initialized .env variables for some settings

--- core/settings.py
+++ core/settings.py
@@ -1,23 +1,14 @@
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eps5z7=5^583lq7r1=%m*s759oup4&$d29a%-s6$wnrent2tb&'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG', 1)
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -27,6 +18,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tickets',
     'users',
+    'api',
+    # DRF
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
